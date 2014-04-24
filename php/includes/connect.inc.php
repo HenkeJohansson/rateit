2d0616ec20f9f6a1bc9850b978fd0cd714ca9c43
Henrik Johansson <henrik89johansson@gmail.com>
2014-04-24 14:40:08 +0200

    Connect-fil

---
+++ php/includes/connect.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+include "password.inc.php";
+
+try {
+    $pdo=new PDO('mysql:host=rateit-193298.phpmyadmin.mysql.binero.se;dbname=193298-rateit','193298_lv84950',$password);
+    $pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec ('SET NAMES "utf8"');
+} catch (Exception $e) {
+    echo "<br>nehedu det gick inte att kontakta databasen<br>";
+    exit ();
+}
+
+?>
