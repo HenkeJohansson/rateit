3a77c895dc66c772ae513cc398ec231007ef6179
Henrik Johansson <henrik89johansson@gmail.com>
2014-04-27 18:08:30 +0200

    sys

--- php/includes/connect.inc.php
+++ php/includes/connect.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-include "password.inc.php";
+include "db_info.inc.php";
 
 try {
-    $pdo=new PDO('mysql:host=rateit-193298.phpmyadmin.mysql.binero.se;dbname=193298-rateit','193298_lv84950',$password);
+    $pdo=new PDO('mysql:host=rateit-193298.phpmyadmin.mysql.binero.se;dbname=$database','$username',$password);
     $pdo->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec ('SET NAMES "utf8"');
 } catch (Exception $e) {
