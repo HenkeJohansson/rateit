398baa513a968945cfa7282cce622ccf41005470
Henrik Johansson <henrik89johansson@gmail.com>
2014-05-06 12:30:48 +0200

    Update Insert
    Funkar

--- insert.php
+++ insert.php
@@ -8,10 +8,10 @@
 	<form action="insert.php" method="post">
 		<input type="text" name="placeName"> placeName <br>
 		<input type="text" name="address"> address <br>
-		<input type="text" name="lat"> lat <br>
-		<input type="text" name="lng"> lng <br>
+		<input type="text" name="lat"> latitude (typ: 55.592941)<br>
+		<input type="text" name="lng"> longitude (typ: 13.010471)<br>
 		<input type="text" name="description"> description <br>
-		<select name="rateing"> rating
+		<select name="rating"> rating
 			<option value="1">1</option>
 			<option value="2">2</option>
 			<option value="3">3</option>
@@ -25,7 +25,7 @@
 			<option value="4">4</option>
 			<option value="5">5</option>
 		</select>
-		<input type="submit" value="Submit">
+		<input type="submit" value="Insert">
 	</form>
 
 	<?php 
@@ -33,10 +33,13 @@
 		require 'php/includes/connect.inc.php';
 
 		if (isset($_POST['placeName'])) {
-			$placeName=htmlspecialchars($_POST['placeName'],ENT_QUOTES);
-			$address=htmlspecialchars($_POST['address'],ENT_QUOTES);
-			$lat=htmlspecialchars($_POST['lat'],ENT_QUOTES);
-			$lng=htmlspecialchars($_POST['lng'],ENT_QUOTES);
+			$placeName = htmlspecialchars($_POST['placeName'],ENT_QUOTES);
+			$address = htmlspecialchars($_POST['address'],ENT_QUOTES);
+			$lat = htmlspecialchars($_POST['lat'],ENT_QUOTES);
+			$lng = htmlspecialchars($_POST['lng'],ENT_QUOTES);
+			$description = htmlspecialchars($_POST['description'],ENT_QUOTES);
+			$rating = $_POST['rating'];
+			$type = $_POST['type'];
 
 
 			$sql="INSERT INTO Places (placeName,address,lat,lng,description,type)
