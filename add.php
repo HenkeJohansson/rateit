e3ea77a7b8d3e64a7f6c2135c809b6dc9025f406
tonnaa <tonyngo_92@hotmail.com>
2014-05-07 22:39:43 +0200

    lägger in data i suggest_places
    funkar dock inte... vet ej varför!!!

--- add.php
+++ add.php
@@ -12,13 +12,13 @@ include $_SERVER['DOCUMENT_ROOT'].'/rateit/php/includes/connect.inc.php'; ?>
                         <p>Känner ni att vi har glömt en plats? Föreslå den gärna till oss då!</p>
                     <br/>
                     <p>Platsens namn</p>
-                        <input type="text" id="insertText" name="placeName">
+                        <input type="text" id="insertText" name="place_name">
                     <p>Beskrivning</p>
-                        <textarea name="info" rows="5" cols="25" id="insertText"> </textarea>
+                        <textarea name="description" rows="5" cols="25" id="insertText"> </textarea>
                     <p>Adress</p>
                         <input type="text" id="insertText" name="address">
                     <p>Typ <i>(Restaurang, pub etc.)</i></p>
-                        <input type="text" id="insertText" name="Typ"> <!--select -> option istället?-->
+                        <input type="text" id="insertText" name="Type"> <!--select -> option istället?-->
                     <p>Betyg (1-5)</p>
                         <input type="rating" id="insertText" name="rating"> <!--select -> option istället?-->
                     </br>
@@ -27,6 +27,31 @@ include $_SERVER['DOCUMENT_ROOT'].'/rateit/php/includes/connect.inc.php'; ?>
     
         </div><!--.bit-3-->
     </div><!--#container-->
+                   <?php
+                
+                    if (isset($_POST['placeName'])){
+                    
+                        //hämta data från formulär
+                        $place_name = htmlspecialchars($_POST['place_name'],ENT_QUOTES);
+                        $description = htmlspecialchars($_POST['description'],ENT_QUOTES);
+                        $address = htmlspecialchars($_POST['address'],ENT_QUOTES);
+                        $type = htmlspecialchars($_POST['type'],ENT_QUOTES);
+                        $rating = htmlspecialchars($_POST['rating'],ENT,QUOTES);
+                        
+                        //lägger in data i databasen
+                        $sql = "INSERT INTO suggest_places (place_name, description, address, type, rating) VALUES (?,?,?,?,?)";
+                        
+                        try {
+                            $statement=$pdo->prepare($sql);
+                            $statement->execute(array($place_name, $description, $address, $type, $rating));
+                        } catch (PDOExeption $e) {
+                            echo "Det funkar ej!";
+                        }
+                    }
+                                
+                    
+               ?>
+ 
 
 	<script src="js/main.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
