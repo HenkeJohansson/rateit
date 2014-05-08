1c63f259cb881b44f01e4eeb55665d24b28e8d2a
tonnaa <tonyngo_92@hotmail.com>
2014-05-08 10:45:11 +0200

    småfel

--- add.php
+++ add.php
@@ -29,7 +29,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/rateit/php/includes/connect.inc.php'; ?>
     </div><!--#container-->
                    <?php
                 
-                    if (isset($_POST['place_Name'])){
+                    if (isset($_POST['place_name'])){
                     
                         //hämta data från formulär
                         $place_name = htmlspecialchars($_POST['place_name'],ENT_QUOTES);
