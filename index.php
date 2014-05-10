7b5ba40d2720974c28b92e321e6502b665a18237
tonnaa <tonyngo_92@hotmail.com>
2014-05-10 13:04:49 +0200

    fixat till menyn så att den funkar

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php include "php/header.php"; ?>
 
-    
 	<div id="map" style="height:100%"></div>
 
     <!-- <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> -->
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 	<script src="js/getPlaces.js"></script>
 	<script src="js/map2.js"></script>
+	<script src="js/main.js"></script>
+
 
 </body>
 </html>
