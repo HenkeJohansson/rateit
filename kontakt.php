a96ce45ad1305fc55c3d0c84ffaf8b524aab12d3
tonnaa <tonyngo_92@hotmail.com>
2014-05-13 19:29:21 +0200

    kontakt-sidan

---
+++ kontakt.php
@@ -0,0 +1,32 @@
+<?php
+//Kontakt
+
+
+//upprätta kontakt med databas
+include 'php/includes/connect.inc.php';?>
+
+<?php include "php/header.php"; ?>
+<div id="map-container"><div id="map" style="height:100%"></div></div>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+	<script src="js/getPlaces.js"></script>
+	<script src="js/map2.js"></script>
+	<script src="js/main.js"></script>
+
+    <div id="container">
+       <div id="om">
+        <h3>Kontakt</h3>   
+            <p><b>Rate It</b></p>
+            <p>Box 50500</p>
+            <p>20250 Malmö</p>
+            <p><b>Telefon:</b> 040-665 70 00</p> 
+            <p><b>Email:</b> info@rateit.se</p>
+            <p><b>Orgnr:</b> 202100-4920</p>
+        
+        
+        </div><!--#searchcontent-->
+    </div><!--#container-->
+
+
+
+</body>
+</html>
